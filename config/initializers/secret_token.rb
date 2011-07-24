dee3948b0f18c742ea10b4ade809593c29525c3a
Chih Yong <wchihyong@gmail.com>
2011-07-24 21:03:41 +0800

    Added mystlye.css

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodOutlets::Application.config.secret_token = '5fa90bb441920a957dc955a534c28cd4f8fe7de368215ed55b33056e4778dd75f508a961e89634d5481e4b32a236ede0f1a18869fd4fce603e99b54c02f55151'
